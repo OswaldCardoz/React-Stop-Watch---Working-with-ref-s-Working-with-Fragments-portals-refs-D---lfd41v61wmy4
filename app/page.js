43d8275eb366116956d36cd0297e313fb3a25bc8
OswaldCardoz <145090621+OswaldCardoz@users.noreply.github.com>
2023-12-08 23:17:40 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,34 +1,72 @@
 'use client'
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 
 function Home() {
-
   const startTime = useRef(0);
   const intervalRef = useRef(0);
+  const lapIndex = useRef(1);
+
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
+
+  const formatTime = (time) => {
+    const formattedTime = (time / 1000).toFixed(3);
+    return formattedTime;
+  };
+
+  const startTimer = () => {
+    startTime.current = Date.now() - currentTime;
+    intervalRef.current = setInterval(() => {
+      setCurrentTime(Date.now() - startTime.current);
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    clearInterval(intervalRef.current);
+  };
+
+  const lapTimer = () => {
+    const lapTime = currentTime;
+    setLaps((prevLaps) => [...prevLaps, lapTime]);
+    lapIndex.current += 1;
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setCurrentTime(0);
+    setLaps([]);
+    lapIndex.current = 1;
+  };
 
   return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
+        <h1 className='seconds-elapsed'>{formatTime(currentTime)}</h1>
         <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+          <button className="start-btn" onClick={startTimer}>
+            START
+          </button>
+          <button className="stop-btn" onClick={stopTimer}>
+            STOP
+          </button>
+          <button className="lap-btn" onClick={lapTimer}>
+            LAP
+          </button>
+          <button className="reset-btn" onClick={resetTimer}>
+            RESET
+          </button>
         </section>
       </section>
-      <section className='lap-section'>
+      <section className={laps.length > 0 ? 'lap-section' : 'lap-section hidden'}>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+          {laps.map((lap, index) => (
+            <p key={index}>{formatTime(lap)}</p>
+          ))}
         </section>
       </section>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
